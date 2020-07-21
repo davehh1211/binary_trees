abfe893b343998fb99169960b613c51280486dda
davehh1211 <davehh1211@gmail.com>
2020-07-21 17:49:26 +0000

    task 9.1 fix count with recursion

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,8 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree->left)
-		count = 1 + binary_tree_height(tree->left);
+		count += 1;
+		binary_tree_height(tree->left);
 	if (tree->right)
-		count = 1 + binary_tree_height(tree->right);
+		count += 1;
+		binary_tree_height(tree->right);
 	return (count);
 }
