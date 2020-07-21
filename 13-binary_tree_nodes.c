5b6b02f87f4a91fede4aa9d9195dfd79be7f074a
davehh1211 <davehh1211@gmail.com>
2020-07-21 06:18:41 +0000

    task 13.0

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes -  function that counts the nodes
+ * with at least 1 child in a binary tree
+ * @tree: is a pointer to the root node of the
+ * tree to count the number of nodes
+ * Return: the number of leaves
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	unsigned int count = 0;
+
+	if (!tree)
+		return (0);
+	if ((tree->left && tree->right))
+		count += 3;
+	else if ((!tree->left && tree->right) || (tree->left && !tree->right))
+		count++;
+	else
+		count = (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	return (count);
+}
