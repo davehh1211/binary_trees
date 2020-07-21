f69f2d3bf1f8ad3807aebe240f457398751b475f
davehh1211 <davehh1211@gmail.com>
2020-07-21 23:07:46 +0000

    task 16.2 fix function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,15 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_is_leaf_2 - a function that checks if a node is a leaf
+ * @node: is a pointer to the node to check
+ * Return: return 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf_2(const binary_tree_t *node)
+{
+	return ((!node || node->left || node->right) ? 0 : 1);
+}
+
+/**
  * binary_tree_height_2 - a function that measures
  * the height of a binary tree
  * @tree:  is a pointer to the root node of the
@@ -12,7 +22,7 @@ size_t binary_tree_height_2(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	else if (!tree->left && !tree->right)
+	else if (binary_tree_is_leaf_2(tree))
 		return (0);
 	left = 1 + binary_tree_height_2(tree->left);
 	right = 1 + binary_tree_height_2(tree->right);
