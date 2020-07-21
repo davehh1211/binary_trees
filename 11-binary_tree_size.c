a406ffefb3f4e05c11573525884efa327c3935ac
davehh1211 <davehh1211@gmail.com>
2020-07-21 18:11:47 +0000

    task 11.1 fix condition and leaf right

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,7 +9,7 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (NULL);
+		return (0);
 
-	return (binary_tree_size(tree->left) + binary_tree_size(tree->left) + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
