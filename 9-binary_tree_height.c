2b0c0c386e00dc341f27b128c3fb2b68cef4bef2
davehh1211 <davehh1211@gmail.com>
2020-07-21 18:09:03 +0000

    task 9.3 fix betty requirement fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,5 +17,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->right)
 		right = 1 + binary_tree_height(tree->right);
 
-	return ((left > right) ? left: right);
+	return ((left > right) ? left : right);
 }
