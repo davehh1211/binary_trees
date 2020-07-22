40516060a74f6c6dea0c658d05aaa4591de9a7b2
davehh1211 <davehh1211@gmail.com>
2020-07-22 00:22:00 +0000

    task 16.4 fix condition into condition

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,6 +49,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 	if (left == right)
-		return (1);
+	{
+		if (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right))
+			return (1);
+	}
 	return (0);
 }
