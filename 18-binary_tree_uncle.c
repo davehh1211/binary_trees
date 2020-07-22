0604de3ee18bf979d4b5b147b7e8392381db9058
davehh1211 <davehh1211@gmail.com>
2020-07-22 22:24:29 +0000

    task 18.8 modified condition

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,8 +6,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent->parent == NULL
-	|| node->parent == NULL || node->parent->parent->left == NULL
+	if (node == NULL || node->parent == NULL
+	|| node->parent->parent == NULL
+	|| node->parent->parent->left == NULL
 	|| node->parent->parent->right == NULL)
 		return (NULL);
 
