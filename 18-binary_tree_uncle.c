3dd9db1a0fc5a1f6ed478cf163c9c8d540bd1e09
davehh1211 <davehh1211@gmail.com>
2020-07-22 22:36:11 +0000

    task 18.10 test other way

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,9 +12,9 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	|| !node->parent->parent->right)
 		return (NULL);
 
-	if (node->parent->parent->left->left == node
-	|| node->parent->parent->left->right == node)
-		return (node->parent->parent->right);
-	else
-		return (node->parent->parent->left);
+	if (node->n > node->parent->parent->n)
+		node = node->parent->parent->left;
+	else if (node->n < node->parent->parent->n)
+		node = node->parent->parent->right;
+	return (node);
 }
